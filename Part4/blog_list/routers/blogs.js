7348980ec27d72132802e3266546469837facb4f
Curtis Anderson <curtisanderson97@hotmail.com>
2021-04-02 23:40:23 +0100

    stable_build

--- Part4/blog_list/routers/blogs.js
+++ Part4/blog_list/routers/blogs.js
@@ -1,5 +1,8 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+require('express-async-errors')
+
+
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -8,20 +11,37 @@ blogsRouter.get('/', async (request, response) => {
       response.json(blogs)
   })
   
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     request.body.likes = 0
     const blog = new Blog(request.body)
     if (blog.url === undefined || blog.title === undefined) {
       response.status(400).end()
     }
   
-    blog
+   const res = await blog
       .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-      .catch(error => null)
+    
+    response.status(201).json(res)
  
   })
+
+blogsRouter.delete('/:id', async (request, response) => {
+
+  await Blog.findByIdAndRemove(request.params.id, (err) => {
+    response.status(404)
+  })
+  
+  response.status(204).end()
+
+  
+  
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const updated = await Blog.findByIdAndUpdate(request.params.id, request.body, {new: true}, (err) => {
+    response.status(404)
+  })
+  response.status(200).json(updated)
+})
 
 module.exports = blogsRouter
--- Part4/blog_list/tests/blog_api.test.js
+++ Part4/blog_list/tests/blog_api.test.js
@@ -5,13 +5,13 @@ const helper = require('../utils/backend_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
+require('express-async-errors')
 
 beforeEach(async () => {
 
     
         
-    try {
+
     await Blog.deleteMany({})
     console.log('cleared')
 
@@ -20,10 +20,7 @@ beforeEach(async () => {
     await Promise.all(promises)
     console.log('saved')
     
-    }
-    catch (error){
-        console.log(error)
-    }
+
 
 })
 
@@ -46,7 +43,24 @@ test('blog had a property called id', async () => {
     expect(response.body[0].id).toBeDefined()
 })
 
-test('blog creates a new blog and adds to database', async () => {
+describe('new blog creations' , () => {
+    test('blog creates a new blog and adds to database', async () => {
+            const response = await api
+                .post('/api/blogs')
+                .send({
+                    title: 'TEST',
+                    author: 'tester',
+                    url: 'test@testing.com',
+                })
+                .expect(201)
+            const newBlogs = await helper.blogsInDb()
+
+            expect(newBlogs).toHaveLength(helper.initialBlogs.length + 1)
+
+            const titles = newBlogs.map(b => b.title)
+            expect(titles).toContain('TEST')
+    })
+    test('blog creates a new blog with default likes set to zero', async () => {
         const response = await api
             .post('/api/blogs')
             .send({
@@ -56,37 +70,68 @@ test('blog creates a new blog and adds to database', async () => {
             })
             .expect(201)
         const newBlogs = await helper.blogsInDb()
+        const createdNote = newBlogs.filter(blog => blog.title === 'TEST')[0].likes
+        expect(createdNote).toBe(0)
+    })
 
-        expect(newBlogs).toHaveLength(helper.initialBlogs.length + 1)
-
-        const titles = newBlogs.map(b => b.title)
-        expect(titles).toContain('TEST')
-})
-test('blog creates a new blog with default likes set to zero', async () => {
-    const response = await api
-        .post('/api/blogs')
-        .send({
-            title: 'TEST',
-            author: 'tester',
-            url: 'test@testing.com',
-        })
-        .expect(201)
-    const newBlogs = await helper.blogsInDb()
-    const createdNote = newBlogs.filter(blog => blog.title === 'TEST')[0].likes
-    expect(createdNote).toBe(0)
-})
-
-test('blog returns 400 error if request body is missing title or url', async() => {
+    test('blog returns 400 error if request body is missing title or url', async() => {
+        
+        const response = await api
+            .post('/api/blogs')
+            .send({
+                author: 'bad request'
+            })
+            .expect(400)
     
-    try { const response = await api
-        .post('/api/blogs')
-        .send({
-            author: 'bad request'
-        })
-        .expect(400)
-    } catch(err) {
-        console.log('error handled')
-    }
+    })
+})
+describe('blog deletions', () => {
+    test('deletes blog with valid id', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        console.log('blogs', blogsAtStart)
+        const blogToDelete = blogsAtStart[0]
+        
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+     
+
+        const blogsAfterDelete = await helper.blogsInDb()
+
+        expect(blogsAfterDelete).toHaveLength(blogsAtStart.length - 1)
+    
+        const results = blogsAfterDelete.map(blog => blog.title)
+        console.log('after', results)
+
+        expect(results).not.toContain(blogToDelete)
+    })
+
+    test('returns 404 Not Found if id is invalid', async () => {
+        await api
+            .delete('/api/blogs/terribleid')
+            .expect(404)
+    })
+
+})
+
+describe('update a blog', () => {
+    test('updates a blog in the database correctly', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToUpdate = blogsAtStart[0]
+
+        const response = await api
+            .put(`/api/blogs/${blogToUpdate.id}`)
+            .send({
+                ...blogToUpdate,
+                likes: 101
+            })
+            .expect(200)
+
+        const blogsAfterUpdate = await helper.blogsInDb()
+        
+        expect(blogsAfterUpdate).toContainEqual(response.body)
+
+    })
 })
 
 afterAll(() => {
